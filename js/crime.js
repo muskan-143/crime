d506a7b4e69a6e9c433099f03ebe16f40f3b0118
muskan-143 <108222164+muskan-143@users.noreply.github.com>
2022-08-22 21:05:19 +0530

    Update crime.js

--- js/crime.js
+++ js/crime.js
@@ -23,31 +23,3 @@ setInterval(() => {
     changeImage();
 }, 2500);
 
-// changeImage();
-
-// for making the loginPage visible when click on the login button.
-// let loginPage = document.getElementById("loginPage");
-// loginPage.style.display = "none";
-// // loginPage.style.position = "absolute";
-// let navigationLoginBtn = document.getElementById("navigationLoginBtn");
-// navigationLoginBtn.addEventListener("click", displayLoginPage);
-
-// // To directly display the login Page when click on the login page.
-// function displayLoginPage() {
-//     console.log("click");
-//     loginPage.style.display = "block";
-//     // window.location.href = "#targetDiv";
-// }
-
-// code to redirect to the login page of the user
-// let userLogin = document.getElementById("userLogin");
-// userLogin.addEventListener("click", () => {
-//     location.href = "../html/login.html";
-// });
-
-// let navigationLoginBtn = document.getElementById("navigationLoginBtn");
-// let loginPage = document.getElementById("loginPage");
-// loginPage.style.visibility = "invisible";
-// navigationLoginBtn.addEventListener("click", () => {
-//     loginPage.style.visibility = "visible";
-// });
